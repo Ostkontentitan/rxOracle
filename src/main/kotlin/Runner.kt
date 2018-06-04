2a0b320f9a2797115258cd2ea01b6720a5bf651b
Johannes Koenen <johannes.koenen@scout24.com>
2018-06-04 10:48:36 +0200

    Stable runner

--- src/main/kotlin/Runner.kt
+++ src/main/kotlin/Runner.kt
@@ -1,4 +1,3 @@
-
 import java.util.concurrent.atomic.AtomicBoolean
 
 object Runner {
@@ -8,21 +7,22 @@ object Runner {
     fun main(args: Array<String>) {
         while (running.get()) {
             Animation.nextFrame()
-            Thread.sleep(150)
+            Thread.sleep(300)
         }
     }
 }
 
 private object Animation {
-    private const val BASE = "(----------)"
+    private const val BASE = "(---------)"
     private const val INNER_LENGTH = 10
 
     private var frame = 0L
 
     fun nextFrame() {
-        val index = ((frame++ % INNER_LENGTH) + 1).toInt()
+        val index = (frame % INNER_LENGTH + 1).toInt()
         val progress = BASE.replaceRange(index, index, "*")
 
         print("\r$progress")
+        frame++
     }
 }
