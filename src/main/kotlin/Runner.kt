67d4e145da6f20d2bef5ff785d76120491f42fac
Johannes Koenen <johannes.koenen@scout24.com>
2018-06-04 09:15:30 +0200

    Gradle + Animation

---
+++ src/main/kotlin/Runner.kt
@@ -0,0 +1,28 @@
+
+import java.util.concurrent.atomic.AtomicBoolean
+
+object Runner {
+    val running = AtomicBoolean(true)
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        while (running.get()) {
+            Animation.nextFrame()
+            Thread.sleep(150)
+        }
+    }
+}
+
+private object Animation {
+    private const val BASE = "(----------)"
+    private const val INNER_LENGTH = 10
+
+    private var frame = 0L
+
+    fun nextFrame() {
+        val index = ((frame++ % INNER_LENGTH) + 1).toInt()
+        val progress = BASE.replaceRange(index, index, "*")
+
+        print("\r$progress")
+    }
+}
