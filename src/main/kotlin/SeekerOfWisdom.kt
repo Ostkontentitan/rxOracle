c525a33ee331c34fc115f7b23d5b3fe8da8717e2
Konstantin Otte <konstantin.otte@scout24.com>
2018-06-13 11:51:42 +0200

    solve task 2

--- src/main/kotlin/SeekerOfWisdom.kt
+++ src/main/kotlin/SeekerOfWisdom.kt
@@ -1,6 +1,19 @@
+import io.reactivex.Single.just
+import io.reactivex.rxkotlin.subscribeBy
+
 object SeekerOfWisdom {
     fun run() {
-        TODO("Challenge 2")
-        Runner.terminate()
+        just(Oracle("Steve"))
+            .flatMap { oracle -> oracle.ask().map { answer -> oracle.name to answer } }
+            .map { (name, answer) -> "$name says $answer" }.subscribeBy(
+                onSuccess = {
+                    println("\n$it")
+                    Runner.terminate()
+                },
+                onError = {
+                    print(it.message)
+                    Runner.terminate()
+                }
+            )
     }
 }
