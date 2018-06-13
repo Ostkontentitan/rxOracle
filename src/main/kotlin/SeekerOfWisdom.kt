c0f6784672a3992af582343e0c10e13893a3f883
Konstantin Otte <konstantin.otte@scout24.com>
2018-06-13 13:22:34 +0200

    solve challenge 4

--- src/main/kotlin/SeekerOfWisdom.kt
+++ src/main/kotlin/SeekerOfWisdom.kt
@@ -1,14 +1,31 @@
+
 import io.reactivex.rxkotlin.subscribeBy
+import kotlin.math.sign
 
 object SeekerOfWisdom {
     fun run() {
         Oracle.stream()
-            .flatMapSingle { oracle -> oracle.ask().map { answer -> oracle.name to answer } }
-            .map { (name, answer) -> "$name says $answer" }.subscribeBy(
-                onNext = {
+            .concatMapSingle { it.ask() } // this part changed to concat for 4b
+            .map {
+                when (it) {
+                    Answer.YES -> 1
+                    Answer.NO -> -1
+                    Answer.MAYBE -> 0
+                }
+            }
+            .reduce(0, { sum, answer -> sum + answer })
+            .map {
+                when (it.sign) {
+                    1 -> Answer.YES
+                    -1 -> Answer.NO
+                    0 -> Answer.MAYBE
+                    else -> throw IllegalStateException("signed result should not deviate from -1,0,1")
+                }
+            }
+            .map { "The oracles answer with: $it" }
+            .subscribeBy(
+                onSuccess = {
                     println("\n$it")
-                },
-                onComplete = {
                     Runner.terminate()
                 },
                 onError = {
